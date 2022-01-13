c97fc8aaa3385227bf9dda25189d8138dc06c7d4
Reante Marrel Patterson <75579137+reantep@users.noreply.github.com>
2022-01-12 22:49:15 -0800

    assesment 4

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SliderPuzzle from './SliderPuzzle';
+
+function App() {
+  return (
+    <div classname="App">
+      <h1>Sliding puzzle</h1>
+      <SliderPuzzle /> 
+    </div>
+    
+  );
+}
+
+export default App;
